d581d1ad99327607b5e5c008f4046b02a1ca401d
Valersd <valersd@abv.bg>
2015-01-20 14:38:18 +0200

    Add web project

---
+++ LibrarySystemMvc.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LibrarySystemMvc.Web.MvcApplication" Language="C#" %>
---
+++ LibrarySystemMvc.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LibrarySystemMvc.Web.Startup))]
+namespace LibrarySystemMvc.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
