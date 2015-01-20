18b8a7f795757823255c1c4c70079cefc693f76c
Valersd <valersd@abv.bg>
2015-01-20 16:56:57 +0200

    Updated EF version to 6.1.2. Installed Glimpse. Created metaclasses and attributes on models.

---
+++ LibrarySystem.Models/Models/Book.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace LibrarySystem.Models
+{
+    [MetadataType(typeof(BookMetadata))]
+    public partial class Book
+    {
+    }
+
+    public class BookMetadata
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(150)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(200)]
+        public string Author { get; set; }
+
+        [StringLength(20)]
+        public string ISBN { get; set; }
+
+        [StringLength(100)]
+        public string Url { get; set; }
+
+        public string Description { get; set; }
+    }
+}
---
+++ LibrarySystem.Models/Models/Category.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace LibrarySystem.Models
+{
+    [MetadataType(typeof(CategoryMetadata))]
+    public partial class Category
+    {
+    }
+
+    public class CategoryMetadata
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        [Index(IsUnique=true)]
+        public string Name { get; set; }
+    }
+}
